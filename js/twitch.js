83bc32ccf0649c75904ce95761b2b1f7ceff9772
Kelly Thao <kjthao@email.sc.edu>
2025-04-03 02:18:03 -0400

    added projects and updated json

--- js/twitch.js
+++
@@ -1,9 +0,0 @@
-const myFunction = () => {
-    const winScroll = window.scrollY;
-    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    const scrolled = (winScroll / height) * 100;
-    document.getElementById("bar").style.width = `${scrolled}%`;
-  };
-  
-  window.onscroll = myFunction;
-  
